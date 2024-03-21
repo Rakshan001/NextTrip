7721efc7ae9fe5d80fc72c0a348b09ee9c13dff1
Rakshan001 <rakshanshetty2003@gmail.com>
2024-03-21 06:14:23 +0530

    save all

--- NextTrip/buytickets/admin.py
+++ NextTrip/buytickets/admin.py
@@ -12,14 +12,45 @@
 
 
 
+
+from django.contrib import admin
+from django.db.models import Sum
+from .models import Ticket
+
+def calculate_total_cost(modeladmin, request, queryset):
+    total_cost = queryset.aggregate(total_cost=Sum('cost')).get('total_cost')
+    if total_cost is not None:
+        message = f'Total Cost of Selected Ticket Bookings: {total_cost}'
+        modeladmin.message_user(request, message)
+    else:
+        modeladmin.message_user(request, 'No ticket bookings selected.')
+
+calculate_total_cost.short_description = 'Calculate Total Cost'
+
+
+
+
 #################################
 
 from django.contrib import admin
 from .models import Ticket
 
 class TicketAdmin(admin.ModelAdmin):
-    list_display = ('id', 'bus', 'origin', 'destination', 'is_verified')
+    list_display = ('id', 'bus', 'origin', 'destination', 'is_verified','cost')
     list_filter = ('bus','date','is_verified')  # Add this line to enable filtering by bus
     list_per_page=10
+    actions = [calculate_total_cost]
 
 admin.site.register(Ticket, TicketAdmin)
+
+
+
+
+
+
+
+
+
+
+
+
