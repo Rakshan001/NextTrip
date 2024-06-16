0ae1a7e541e3bb1d126a9af40401ab70db014727
Rakshan001 <rakshanshetty2003@gmail.com>
2024-06-16 10:19:59 +0530

    save

--- NextTrip/NextTrip/wsgi.py
+++ NextTrip/NextTrip/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NextTrip.settings')
 
 application = get_wsgi_application()
+
+app=application  #it is used for deployment in vercel   
